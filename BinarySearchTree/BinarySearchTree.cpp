86bac1095c09aa0dd458ae8685a2e93af69ed3a7
muhfarrasm <muhammadfarrasm@gmail.com>
2023-06-29 19:51:21 +0700

    Membuat loop while ((currentNode != NULL) && (currentNode->info != element))

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -59,5 +59,12 @@ public:
 		//This function search the currentNode of the specified Node as well as the current Node of its parent
 		currentNode = ROOT;
 		parent = NULL;
+		while ((currentNode != NULL) && (currentNode->info != element)) {
+			parent = currentNode;
+			if (element < currentNode->info)
+				currentNode = currentNode->leftchild;
+			else
+				currentNode = currentNode->rightchild;
+		}
 	}
 };
