cc200654aa44b31247a2567fdefd03e758f2ddf1
muhfarrasm <muhammadfarrasm@gmail.com>
2023-06-29 19:49:53 +0700

    inisialisasi currentNode = ROOT;

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -57,6 +57,6 @@ public:
 	void search(string element, Node*& parent, Node*& currentNode) 
 	{
 		//This function search the currentNode of the specified Node as well as the current Node of its parent
-
+		currentNode = ROOT;
 	}
 };
